a82aecacec7090a0e0bc403c3596593fa3ed23b0
Andrew Carr <andrewcarr@Andrews-MBP.veracitynetworks.net>
2016-12-08 18:56:57 -0700

    revert change

--- mathify/interpolate/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-
-class NameForm(forms.Form):
-    text_input = forms.CharField(label='Your name', max_length=100)
-    silly = forms.BooleanField(label="silly")
-    scribble = forms.BooleanField(label="scribble")
-
